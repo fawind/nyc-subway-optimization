a3462bddebb9fde7eff15b76f89743811eef9f3c
fawind <windheuser.fabian@gmail.com>
2015-05-26 15:50:57 +0200

    Fix routes

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,4 @@ from app.handlers.api import *
 class Routes():
     def add_routes(api):
         # Add backend routes here
-        api.add_resource(TestPage, '/api/tes')
+        api.add_resource(TestPage, '/api/test')
