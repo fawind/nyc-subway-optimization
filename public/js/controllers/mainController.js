a0ad85e839fe36f1a48058e0def05b9f3408e99f
fawind <windheuser.fabian@gmail.com>
2015-05-26 20:52:26 +0200

    Add map directive

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('epic-taxi')
+angular.module('epic-taxi', ['leaflet-directive'])
   .controller('MainController', ['$scope', 'MainService', function ($scope, mainService) {
     $scope.test = 'Hello Angular';
 
@@ -9,5 +9,19 @@ angular.module('epic-taxi')
       .success(function(results) {
         console.log(results);
       });
+
+    angular.extend($scope, {
+      newYork: {
+        lat: 40.7304783951045,
+        lng: -73.98880004882812,
+        zoom: 13
+      },
+      tiles: {
+        url: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+        options: {
+          attribution: '<a href="http://cartodb.com/attributions">CartoDB</a>'
+        }
+      }
+    });
 
   }]);
