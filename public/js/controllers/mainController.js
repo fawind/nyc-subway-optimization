b1132ae7e59cf9221d2aed73dd4db17805ad32fb
fawind <windheuser.fabian@gmail.com>
2015-06-08 16:43:29 +0200

    Fix intending

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,169 +1,167 @@
 angular.module('epic-taxi')
-.controller('MainController', ['$scope', 'lodash', 'MainService', 'leafletEvents', function ($scope, _, mainService, leafletEvents) {
+  .controller('MainController', ['$scope', 'lodash', 'MainService', 'leafletEvents', function ($scope, _, mainService, leafletEvents) {
 
-  var markers = {};
-  var paths = {};
-  var stationIcon = {
-    iconUrl: 'assets/station-marker2.png',
-    iconSize: [13, 13]
-  };
+    var markers = {};
+    var paths = {};
+    var stationIcon = {
+      iconUrl: 'assets/station-marker2.png',
+      iconSize: [13, 13]
+    };
 
-  var iconScale = d3.scale.sqrt()
-  .domain([6, 8, 10, 12, 13, 16, 18])
-  .range([1, 3, 7, 13, 16, 35, 50]);
+    var iconScale = d3.scale.sqrt()
+      .domain([6, 8, 10, 12, 13, 16, 18])
+      .range([1, 3, 7, 13, 16, 35, 50]);
 
-  var pathScale = d3.scale.sqrt()
-  .domain([9, 12, 14, 16, 18])
-  .range([1, 3, 5, 8, 12]);
+    var pathScale = d3.scale.sqrt()
+      .domain([9, 12, 14, 16, 18])
+      .range([1, 3, 5, 8, 12]);
 
-  $scope.initMap = function() {
-    angular.extend($scope, {
-      newYork: {
-        lat: 40.7304783951045,
-        lng: -73.98880004882812,
-        zoom: 12
-      },
-      subwayPaths: {},
-      layers: {
-        baselayers: {
-          mapbox_light: {
-            name: 'Light',
-            type: 'xyz',
-            url: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+    $scope.initMap = function() {
+      angular.extend($scope, {
+        newYork: {
+          lat: 40.7304783951045,
+          lng: -73.98880004882812,
+          zoom: 12
+        },
+        subwayPaths: {},
+        layers: {
+          baselayers: {
+            mapbox_light: {
+              name: 'Light',
+              type: 'xyz',
+              url: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+            },
+            mapbox_dark: {
+              name: 'Dark',
+              type: 'xyz',
+              url: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
+            }
           },
-          mapbox_dark: {
-            name: 'Dark',
-            type: 'xyz',
-            url: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
+          overlays: {
+            subway: {
+              name: 'Subway',
+              visible: true,
+              type: 'group',
+            }
           }
         },
-        overlays: {
-          subway: {
-            name: 'Subway',
-            visible: true,
-            type: 'group',
+        events: {
+          markers: {
+            enable: [leafletEvents.click, leafletEvents.popupclose]
           }
+        },
+        defaults: {
+          layerControl: true
         }
-      },
-      events: {
-        markers: {
-          enable: [leafletEvents.click, leafletEvents.popupclose]
-        }
-      },
-      defaults: {
-        layerControl: true
-      }
-    });
-
-    $scope.$on('leafletDirectiveMarker.click', function(event, args) {
-      $scope.hideSubway(args.model.stationId);
-      mainService.getCluster(args.model.stationId)
-      .success(function(response) {
-        console.log(response);
       });
-    });
 
-    $scope.$on('leafletDirectiveMarker.popupclose', function(event, args) {
-      $scope.showSubway();
-    });
-
-    $scope.$on('leafletDirectiveMap.zoomend', function(event, args){
-      var zoomLevel = args.leafletEvent.target._zoom;
-      updateRoutesAndStationIcons(zoomLevel);
-    });
-  };
-
-  $scope.hideSubway = function(id) {
-    _.each($scope.markers, function(marker){
-      if (id !== marker.stationId)
-        marker.opacity = 0.2;
-    });
-    _.each($scope.subwayPaths, function(path){
-      path.opacity = 0.2;
-    });
-  };
-
-  $scope.showSubway = function() {
-    _.each($scope.markers, function(marker){
-      marker.opacity = 1.0;
-    });
-    _.each($scope.subwayPaths, function(path){
-      path.opacity = 1.0;
-    });
-  };
-
-  $scope.addMarkers = function() {
-    angular.extend($scope, {
-      markers: angular.copy(markers)
-    });
-  };
-
-  $scope.addPaths = function() {
-    angular.extend($scope, {
-      subwayPaths: angular.copy(paths)
-    });
-  };
-
-  function addStations(routes) {
-    _.each(routes, function(route) {
-      _.each(route.stations, function(station, i) {
-        markers[station.id] = {
-          stationId: station.id,
-          lat: parseFloat(station.lat),
-          lng: parseFloat(station.lng),
-          message: station.name,
-          focus: false,
-          draggable: false,
-          icon: stationIcon,
-          layer: 'subway'
-        };
-      });
-    });
-    $scope.addMarkers();
-  }
-
-  function addRoutes(routes) {
-    _.each(routes, function(route) {
-      var routePath = {
-        color: mainService.getRouteColor(route.route),
-        weight: 3,
-        message: route.route,
-        latlngs: [],
-        layer: 'subway'
-      };
-
-      _.each(route.stations, function(station) {
-        routePath.latlngs.push({
-          lat: parseFloat(station.lat),
-          lng: parseFloat(station.lng)
+      $scope.$on('leafletDirectiveMarker.click', function(event, args) {
+        $scope.hideSubway(args.model.stationId);
+        mainService.getCluster(args.model.stationId)
+        .success(function(response) {
+          console.log(response);
         });
       });
 
-      paths[route.route] = routePath;
-    });
-    $scope.addPaths();
-  }
+      $scope.$on('leafletDirectiveMarker.popupclose', function(event, args) {
+        $scope.showSubway();
+      });
 
-  function updateRoutesAndStationIcons(zoomLevel) {
+      $scope.$on('leafletDirectiveMap.zoomend', function(event, args){
+        var zoomLevel = args.leafletEvent.target._zoom;
+        updateRoutesAndStationIcons(zoomLevel);
+      });
+    };
 
+    $scope.hideSubway = function(id) {
+      _.each($scope.markers, function(marker){
+        if (id !== marker.stationId)
+          marker.opacity = 0.2;
+      });
+      _.each($scope.subwayPaths, function(path){
+        path.opacity = 0.2;
+      });
+    };
 
-    _.each($scope.markers, function(marker) {
-      marker.icon.iconSize = [iconScale(zoomLevel), iconScale(zoomLevel) ];
-    });
+    $scope.showSubway = function() {
+      _.each($scope.markers, function(marker){
+        marker.opacity = 1.0;
+      });
+      _.each($scope.subwayPaths, function(path){
+        path.opacity = 1.0;
+      });
+    };
 
-    _.each($scope.subwayPaths, function(path) {
-      path.weight = pathScale(zoomLevel);
-    });
-  }
+    $scope.addMarkers = function() {
+      angular.extend($scope, {
+        markers: angular.copy(markers)
+      });
+    };
+
+    $scope.addPaths = function() {
+      angular.extend($scope, {
+        subwayPaths: angular.copy(paths)
+      });
+    };
+
+    function addStations(routes) {
+      _.each(routes, function(route) {
+        _.each(route.stations, function(station, i) {
+          markers[station.id] = {
+            stationId: station.id,
+            lat: parseFloat(station.lat),
+            lng: parseFloat(station.lng),
+            message: station.name,
+            focus: false,
+            draggable: false,
+            icon: stationIcon,
+            layer: 'subway'
+          };
+        });
+      });
+      $scope.addMarkers();
+    }
+
+    function addRoutes(routes) {
+      _.each(routes, function(route) {
+        var routePath = {
+          color: mainService.getRouteColor(route.route),
+          weight: 3,
+          message: route.route,
+          latlngs: [],
+          layer: 'subway'
+        };
+
+        _.each(route.stations, function(station) {
+          routePath.latlngs.push({
+            lat: parseFloat(station.lat),
+            lng: parseFloat(station.lng)
+          });
+        });
+
+        paths[route.route] = routePath;
+      });
+      $scope.addPaths();
+    }
+
+    function updateRoutesAndStationIcons(zoomLevel) {
+      _.each($scope.markers, function(marker) {
+        marker.icon.iconSize = [iconScale(zoomLevel), iconScale(zoomLevel) ];
+      });
+
+      _.each($scope.subwayPaths, function(path) {
+        path.weight = pathScale(zoomLevel);
+      });
+    }
 
     // init the Map and load the subway routes
     $scope.initMap();
 
     mainService.getStations()
-    .success(function(routes) {
-      console.log('Got all stations...');
-      addRoutes(routes);
-      addStations(routes);
-    });
+      .success(function(routes) {
+        console.log('Got all stations...');
+        addRoutes(routes);
+        addStations(routes);
+      });
 
   }]);
