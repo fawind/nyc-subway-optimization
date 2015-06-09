4bdffe6121770244bf86681822264ac02b34cc13
fawind <windheuser.fabian@gmail.com>
2015-06-10 00:34:30 +0200

    Add first version of leaflet-d3-directive

--- public/js/directives/subwayRoutes.js
+++ public/js/directives/subwayRoutes.js
@@ -1,9 +1,34 @@
 angular.module('epic-taxi')
-  .directive("subway", function() {
+  .directive("cluster", function() {
+    // cluster: {'lat' : '123', 'lng': '123'}
+
     return {
       require: 'leaflet',
-      link: function(scope, element, routes, leafletController) {
-        console.log('scope: ' + leafletController.getLeafletScope());
+      scpope: 'false',
+      replace: 'false',
+      link: function(scope, element, attrs, leafletController) {
+
+        var svg = d3.select(element[0])
+              .append("svg");
+
+        scope.$watch('cluster', function(newCluster, oldCluster) {
+          if (!newCluster) return;
+          return scope.render(newCluster);
+        });
+
+        scope.render = function(cluster) {
+          var data =  [ {LatLng: new L.LatLng(cluster.lat, cluster.lng)} ];
+
+          svg.selectAll('*').remove();
+
+          svg.selectAll('circle')
+            .data(data)
+            .enter().append('circle')
+            .style("stroke", "black")
+            .style("opacity", 0.6)
+            .style("fill", "red")
+            .attr("r", 20);
+        };
       }
     };
   });
