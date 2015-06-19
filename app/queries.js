57f45d927e379784b55fb9d7d1c7cdeb4eb56b03
Alexander Immer <alexander.immer@yahoo.com>
2015-06-20 01:31:07 +0200

    Finished migration (only required queries)

--- app/queries.js
+++ app/queries.js
@@ -2,7 +2,7 @@ var client = require('./hana');
 var geo = require('./utils/geo');
 
 var QueryHandler = {
-  get_entry: function(lat_st, lng_st, raster_deg) {
+  get_cluster: function(lat_st, lng_st, raster_deg, cb) {
     client.connect(function (err) {
       if (err) {
         return console.error('Connect error', err);
@@ -23,7 +23,7 @@ var QueryHandler = {
       while(lat < geo.get_top_left.lat) {
         // start in the west of NYC
         while(lng < geo.get_bottom_right.lng) {
-          query_list.push('SELECT COUNT(ID), '+String(lat)+', '+String(lng)+
+          query_list.push('SELECT COUNT(ID) AS "count", '+String(lat)+' as "lat", '+String(lng)+'as "lng"'+
             ' FROM NYCCAB.TRIP WHERE '+base_pickup+
             ' AND DROPOFF_LONG < '+String(lng + offset_lng)+' AND DROPOFF_LONG > '+String(lng - offset_lng)+
             ' AND DROPOFF_LAT < '+String(lat + offset_lat)+' AND DROPOFF_LAT > '+String(lat - offset_lat));
@@ -39,27 +39,65 @@ var QueryHandler = {
 
       var query = query_list.join(' UNION ALL ');
 
+      console.log('DB Start Query');
+
       client.exec(query, function (err, rows) {
         client.end();
         if (err) {
           return console.error('Execute error:', err);
         }
-        console.log('Results:', rows);
+        console.log('DB Finished Query')
+        cb(rows);
       });
     });
   },
-  get_cluster: function() {
+
+  get_cluster_filtered: function(lat_st, lng_st, raster_deg, cb) {
     client.connect(function (err) {
       if (err) {
         return console.error('Connect error', err);
       }
 
-      client.exec('SELECT COUNT(TOTAL) FROM NYCCAB.FARE WHERE TOTAL > 0', function (err, rows) {
+      var offset_lng = geo.get_lng_diff / (raster_deg * 2)
+      var offset_lat = geo.get_lat_diff / (raster_deg * 2)
+
+      var lat = geo.get_bottom_right.lat + offset_lat
+      var lng = geo.get_top_left.lng + offset_lng
+
+      var base_pickup = 'PICKUP_LONG < '+String(lng_st + offset_lng)+' AND PICKUP_LONG > '+String(lng_st - offset_lng)+
+        ' AND PICKUP_LAT < '+String(lat_st + offset_lat)+' AND PICKUP_LAT > '+String(lat_st - offset_lat);
+
+
+      var query_list = [];
+      // start in the south of NYC
+      while(lat < geo.get_top_left.lat) {
+        // start in the west of NYC
+        while(lng < geo.get_bottom_right.lng) {
+          query_list.push('SELECT COUNT(ID) AS "count", '+String(lat)+' as "lat", '+String(lng)+'as "lng"'+
+            ' FROM NYCCAB.TRIP WHERE '+base_pickup+
+            ' AND DROPOFF_LONG < '+String(lng + offset_lng)+' AND DROPOFF_LONG > '+String(lng - offset_lng)+
+            ' AND DROPOFF_LAT < '+String(lat + offset_lat)+' AND DROPOFF_LAT > '+String(lat - offset_lat));
+
+          // increase longitude for next iteration by one box-size
+          lng = lng + 2 * offset_lng
+        };
+        // increase latitude for next iteration by one box-size
+        lat = lat + 2 * offset_lat
+        // reset longitude
+        lng = geo.get_top_left.lng
+      };
+
+      var query = query_list.join(' UNION ALL ');
+
+      console.log('DB Start Query');
+
+      client.exec(query, function (err, rows) {
         client.end();
         if (err) {
           return console.error('Execute error:', err);
         }
-        console.log('Results:', rows);
+        console.log('DB Finished Query');
+        cb(rows);
       });
     });
   }
--- app/routes.js
+++ app/routes.js
@@ -1,17 +1,21 @@
 var express = require('express');
-var router = express.Router();
 var QueryHandler = require('./queries');
 
+var router = express.Router();
+
 router.post('/api/cluster', function(req, res, next) {
-  console.log('POST request to ' + req.url);
-  console.log(req.body);
-  QueryHandler.get_entry(req.body.lat, req.body.lng, 10);
-  res.json()
+  console.log('POST request to ' + req.url + ' Station: ' + req.body.id);
+
+  QueryHandler.get_cluster_filtered(req.body.lat, req.body.lng, 10, function(rows){
+  	res.json({cluster: rows, raster: 10})
+  });
+
 });
 
 router.post('/api/clusterFiltered', function(req, res, next) {
   console.log('POST request to ' + req.url);
   console.log(req.body);
+  var result = QueryHandler.get_cluster_filtered()
   res.json()
 });
 
