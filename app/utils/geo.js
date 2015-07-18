3d67de77a4fce8c90d53a25b04e4f39d58903661
Alexander Immer <alexander.immer@yahoo.com>
2015-07-19 01:26:13 +0200

    documented geo

--- app/utils/geo.js
+++ app/utils/geo.js
@@ -1,10 +1,18 @@
-// spans MAX area we focus on manhatten area and east till JFK-airport
+// spans MAX area. We focus on manhatten area and east till JFK-airport
 const TopLeft = {lng: -74.019760, lat: 40.864695};
 const BottomRight = {lng: -73.779058, lat: 40.621053};
 const defaultLNG = -74.019760;
 const defaultLAT = 40.864695;
 
 var GeoUtils = {
+  /**
+   * calculate the distance between to spatial points
+   * @param {Number} latitude point a
+   * @param {Number} longitude point a
+   * @param {Number} latitude point b
+   * @param {Number} longitude point b
+   * @return {Number} distance in meters
+   */
   getDistance_m: function(lat1, lng1, lat2, lng2) {
     var radius = 6371000;
 
@@ -26,13 +34,27 @@ var GeoUtils = {
     return deg * (Math.PI/180);
   },
 
+  /**
+   * calculate relative difference for lat-value to add a value in meters
+   * @param {Number} latitude of point
+   * @param {Number} longitude of point
+   * @param {Number} distance between lat-values
+   * @return {Number} Lat-diff approximately fitting blockSize_m in spatial
+   */
   getLatDiff: function(lat1, lat2, blockSize_m) {
-    var d = this.getDistance_m(lat1, defaultLNG, lat2, defaultLNG);
+    var d = GeoUtils.getDistance_m(lat1, defaultLNG, lat2, defaultLNG);
     return Math.abs(lat1 - lat2) * (blockSize_m/d);
   },
 
+  /**
+   * calculate relative difference for lng-value to add a value in meters
+   * @param {Number} latitude of point
+   * @param {Number} longitude of point
+   * @param {Number} distance between lat-values
+   * @return {Number} Lng-diff approximately fitting blockSize_m in spatial
+   */
   getLngDiff: function(lng1, lng2, blockSize_m) {
-    var d = this.getDistance_m(defaultLAT, lng1, defaultLAT, lng2);
+    var d = GeoUtils.getDistance_m(defaultLAT, lng1, defaultLAT, lng2);
     return Math.abs(lng1 -lng2) * (blockSize_m/d);
   }
 };
