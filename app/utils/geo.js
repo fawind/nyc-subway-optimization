192084eb76960250c7160c4bf12a4e7e7304ab12
Alexander Immer <alexander.immer@yahoo.com>
2015-07-19 00:21:19 +0200

    refactor geo

--- app/utils/geo.js
+++ app/utils/geo.js
@@ -1,30 +1,10 @@
+// spans MAX area we focus on manhatten area and east till JFK-airport
+const TopLeft = {lng: -74.019760, lat: 40.864695};
+const BottomRight = {lng: -73.779058, lat: 40.621053};
+const defaultLNG = -74.019760;
+const defaultLAT = 40.864695;
+
 var GeoUtils = {
-  // spans MAX area we focus on manhatten area and east till JFK-airport
-  getTopLeft: {lng: -74.019760, lat: 40.864695},
-  getBottomRight: {lng: -73.779058, lat: 40.621053},
-
-  getLatDiff_sph: 0.243643,
-  getLngDiff_sph: 0.240702,
-
-  // return quite accurate value for border of our rectangle
-  getLatDiff_m: function() {
-    return this.getDistance_m(this.getTopLeft.lat, this.getTopLeft.lng,
-      this.getBottomRight.lat,this.getTopLeft.lng);
-  },
-
-  getLngDiff_m: function() {
-    return this.getDistance_m(this.getTopLeft.lat, this.getTopLeft.lng,
-      this.getTopLeft.lat,this.getBottomRight.lng);
-  },
-
-  getLatDiff_n: function() {
-    return Math.abs(this.getTopLeft.lat - this.getBottomRight.lat);
-  },
-
-  getLngDiff_n: function() {
-    return Math.abs(this.getTopLeft.lng - this.getBottomRight.lng);
-  },
-
   getDistance_m: function(lat1, lng1, lat2, lng2) {
     var radius = 6371000;
 
@@ -47,14 +27,12 @@ var GeoUtils = {
   },
 
   getLatDiff: function(lat1, lat2, blockSize_m) {
-    var d = this.getDistance_m(lat1, this.getTopLeft.lng,
-      lat2, this.getTopLeft.lng);
+    var d = this.getDistance_m(lat1, defaultLNG, lat2, defaultLNG);
     return Math.abs(lat1 - lat2) * (blockSize_m/d);
   },
 
   getLngDiff: function(lng1, lng2, blockSize_m) {
-    var d = this.getDistance_m(this.getTopLeft.lat, lng1,
-      this.getTopLeft.lat, lng2);
+    var d = this.getDistance_m(defaultLAT, lng1, defaultLAT, lng2);
     return Math.abs(lng1 -lng2) * (blockSize_m/d);
   }
 };
